551f1123de25af08b6ae1b7f8641b1472b0d036c
Jose Amezola Beltran <jose.amezola.beltran@gmail.com>
2018-02-11 20:31:30 -0700

    node hw
    node hw

---
+++ keys.js
@@ -0,0 +1,11 @@
+console.log("Keys loaded");
+
+
+var twitterKeys = {
+  consumer_key: "MrEzhqGD8geCi4J1B4fTVSlKM",
+  consumer_secret:"IFh0jrz7b2GjOnXdnv0hZTkZBCjWJsHjesEdo4suL6ZK9Rua2a" ,
+  access_token_key:	"962883715478167552-PxTzO08QVhgnEpWmPuJeqgBjb3ck20z" ,
+  access_token_secret: "	on3Vzi9bS0fPmxIeRMDwxTeuPWerp2Hvo8CplLbKhqLUg"
+};
+
+module.exports = twitterKeys;
